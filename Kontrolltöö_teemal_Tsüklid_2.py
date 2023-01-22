fc8e9f341a93af8e1c9a363e92902acbe63cff05
milju <milju@MSI>
2023-01-22 23:21:17 +0200

    Добавьте файлы проекта.

---
+++ Kontrolltöö_teemal_Tsüklid_2.py
@@ -0,0 +1,52 @@
+﻿from math import *
+from random import *
+
+#1
+
+#n=randint(1,9)
+#print(n)
+#while n!=0: 
+#    print("   (\_/)")
+#    print("   (o o)")
+#    print("   / | \*")
+#    n-=1
+
+#2
+#L=int(input("Sisestage number L: "))
+#summ = 0
+#for i in range(L+1):
+#    summ += i
+#print("Numnbrite summa 0 kuni L on: ", summ)
+
+
+#3
+#n=0
+#number=randint(0,100)
+#while n<10:
+#   b=int(input("number:  "))
+#   if b>number:
+#       print("väiksem")
+#   elif b<number:
+#       print("rohkem")
+#   elif b==number:
+#       print("Arvasid numbri ära!")
+#       break    
+#   n+=1 
+#print("See on number " + str(number)) 
+
+#4
+
+
+#5
+
+#print("sisesta number")
+#n = int(input())
+#summa = 0
+#korrutis = 1
+#while n > 0:
+#    digit = n % 10
+#    summa = summa + digit
+#    korrutis = korrutis * digit
+#    n = n // 10
+#print("summa:", summa)
+#print("korrutis:", korrutis)
